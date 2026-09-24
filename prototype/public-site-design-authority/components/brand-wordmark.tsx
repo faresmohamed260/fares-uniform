0f25f8796642b85e23d20d520522668d011058b7
Fares Mohamed <121525183+faresmohamed260@users.noreply.github.com>
2026-09-25 02:36:40 +0300

    feat(public-site): replace draft marks with Fares-supplied logos

--- prototype/public-site-design-authority/components/brand-wordmark.tsx
+++ prototype/public-site-design-authority/components/brand-wordmark.tsx
@@ -1,6 +1,3 @@
 export function BrandWordmark(){
-  return <svg className="fares-wordmark" viewBox="0 0 250 90" role="img" aria-label="Fares Uniform" xmlns="http://www.w3.org/2000/svg">
-    <text className="fares-wordmark-main" x="125" y="57" textAnchor="middle">FARES</text>
-    <text className="fares-wordmark-sub" x="125" y="82" textAnchor="middle">UNIFORM</text>
-  </svg>;
+  return <img className="fares-wordmark" src="/design-media/fares-uniform-wordmark.png" width="1200" height="404" alt="Fares Uniform" />;
 }
